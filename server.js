be1de8879c366f1e497a64c29220a776eab300a6
Jonathan Funk <jonfunk88@hotmail.com>
2018-06-24 13:12:44 -0700

    route files created

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI; //Database Config
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+//Database Config
+const db = require('./config/keys').mongoURI;
 
 //Conntect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("It's working!"));
+app.get('/', (req, res) => res.send("It's working!"));
+
+//Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
