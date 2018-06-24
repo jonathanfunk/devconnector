bec8b114702fe8d3a08d5b15d716a6b22c27eca9
Jonathan Funk <jonfunk88@hotmail.com>
2018-06-24 12:40:45 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI; //Database Config
 
 const app = express();
+
+//Conntect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("It's working!"));
 
