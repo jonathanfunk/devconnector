c3cb3ad1e690d708c185fbf9ad66f8ef82db3c8d
Jonathan Funk <jonfunk88@hotmail.com>
2018-06-24 18:26:02 -0700

    jwt added to login route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 // Import APIs
 const users = require('./routes/api/users');
