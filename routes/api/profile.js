00d4d62e268a764ef60a8e21cbbc2249ee620902
Jonathan Funk <jonfunk88@hotmail.com>
2018-06-27 21:52:34 -0700

    delete routes for profiles, users, education & experience created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -230,4 +230,72 @@ router.post(
   }
 );
 
+//@Route  DELETE api/profile/experience/:exp_id
+//@Desc   Delete experience from profile
+//@Access Private
+
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      //Get remove index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+
+      // Splice out of array
+      profile.experience.splice(removeIndex);
+
+      // Save
+      profile
+        .save()
+        .then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+  }
+);
+
+//@Route  DELETE api/profile/education/:edu_id
+//@Desc   Delete education from profile
+//@Access Private
+
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      //Get remove index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+
+      // Splice out of array
+      profile.education.splice(removeIndex);
+
+      // Save
+      profile
+        .save()
+        .then(profile => res.json(profile))
+        .catch(err => res.status(404).json(err));
+    });
+  }
+);
+
+//@Route  DELETE api/profile/education/:edu_id
+//@Desc   Delete user and profile
+//@Access Private
+
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() => {
+        res.json({ success: true });
+      });
+    });
+  }
+);
+
 module.exports = router;
