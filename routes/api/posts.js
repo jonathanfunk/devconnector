108257dc4202fed34d19365b4247d1b93407c02b
Jonathan Funk <jonfunk88@hotmail.com>
2018-07-10 18:41:17 -0700

    ability to get or delete posts created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,8 +6,9 @@ const passport = require('passport');
 // Load Validation
 const validatePostInput = require('./../../validation/post');
 
-// Load Post Model
+// Load Models
 const Post = require('./../models/Post');
+const Profile = require('./../models/Profile');
 
 //@Route  GET api/posts/test
 //@Desc   Tests posts route
@@ -19,6 +20,29 @@ router.get('/test', (req, res) =>
 );
 
 //@Route  GET api/posts
+//@Desc   Get posts
+//@Access Public
+
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found' }));
+});
+
+//@Route  GET api/posts/:id
+//@Desc   Get single post
+//@Access Public
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
+    );
+});
+
+//@Route  POST api/posts
 //@Desc   Create post
 //@Access Private
 
@@ -40,6 +64,29 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+//@Route  DELETE api/posts/:id
+//@Desc   Delete post
+//@Access Private
+
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+    });
   }
 );
 
