af42138da66d3d5cc99a07e3595b90c6f1ef6743
Jonathan Funk <jonfunk88@hotmail.com>
2018-07-11 21:37:56 -0700

    ability to like posts added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dev Connector
+
+Work in progress
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -90,4 +90,66 @@ router.delete(
   }
 );
 
+//@Route  POST api/posts/like:id
+//@Desc   Like post
+//@Access Private
+
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User already liked this post' });
+          }
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+    });
+  }
+);
+
+//@Route  POST api/posts/unlike:id
+//@Desc   Unlike post
+//@Access Private
+
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: 'You have not yet liked this post' });
+          }
+          //Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          //Splice out array
+          post.likes.splice(removeIndex, 1);
+
+          //Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'Post not found' }));
+    });
+  }
+);
+
 module.exports = router;
