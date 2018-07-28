4e2c2c4e0b28494b08f7bd7ef6da00361c563700
Jonathan Funk <jonfunk88@hotmail.com>
2018-07-27 21:13:43 -0700

    current profile reducer made

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from './../../actions/authActions';
+import { clearCurrentProfile } from './../../actions/profileActions';
 
 class Navbar extends Component {
   onLogoutClick = e => {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   };
 
@@ -16,17 +18,13 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a 
-            href="" 
-            onClick={this.onLogoutClick} 
-            className="nav-link"
-          >
-            <img 
-              className='rounded-circle'
-              src={user.avatar} 
-              alt={user.name} 
-              style={{ width: '25px', marginRight: '5px'}} 
-              title="You must have a Gravatar connected to your email to display an image" 
+          <a href="" onClick={this.onLogoutClick} className="nav-link">
+            <img
+              className="rounded-circle"
+              src={user.avatar}
+              alt={user.name}
+              style={{ width: '25px', marginRight: '5px' }}
+              title="You must have a Gravatar connected to your email to display an image"
             />
             Logout
           </a>
@@ -91,5 +89,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
