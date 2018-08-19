4a2a7653b37eec0a48109abe5ae8f941aaa54c28
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-19 15:22:10 -0700

    added personal site link to footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector
+      Copyright &copy; {new Date().getFullYear()} DevConnector. Developed by{' '}
+      <a className="text-white" href="http://www.jon-funk.com" target="_blank">
+        Jonathan Funk
+      </a>
+      .
     </footer>
   );
 };
