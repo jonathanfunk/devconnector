bec6f21f16a8de0b4a14c7ac88ecdd6558d94c9f
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-19 13:46:01 -0700

    post comments are now visible

---
+++ client/src/components/post/CommentFeed.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CommentItem from './CommentItem';
+
+class CommentFeed extends Component {
+  render() {
+    const { comments, postId } = this.props;
+
+    return comments.map(commentItem => (
+      <CommentItem
+        key={commentItem._id}
+        comment={commentItem}
+        postId={postId}
+      />
+    ));
+  }
+}
+
+CommentFeed.propTypes = {
+  comments: PropTypes.array.isRequired,
+  postId: PropTypes.string.isRequired
+};
+
+export default CommentFeed;
