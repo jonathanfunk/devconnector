49c6687a62c27b220266b4f4a7ab1cc4c412e0b7
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-19 16:09:34 -0700

    prepare for deployment

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,7 +4,12 @@ export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} DevConnector. Developed by{' '}
-      <a className="text-white" href="http://www.jon-funk.com" target="_blank">
+      <a
+        className="text-white"
+        href="http://www.jon-funk.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Jonathan Funk
       </a>
       .
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // Import APIs
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const app = express();
 
