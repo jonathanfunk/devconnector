ed00238060524203dc3c75db7cc622fcc12fa4c8
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-19 12:34:52 -0700

    started on the single post page

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
+  GET_POST,
   POST_LOADING,
   DELETE_POST
 } from './types';
@@ -24,6 +25,7 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
 //Get Posts
 export const getPosts = () => dispatch => {
   dispatch(setPostLoading());
@@ -38,6 +40,25 @@ export const getPosts = () => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//Get Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
         payload: null
       })
     );
---
+++ client/src/components/post/Post.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import PostItem from './../posts/PostItem';
+import Spinner from './../common/Spinner';
+import { getPost } from './../../actions/postActions';
+
+class Post extends Component {
+  componentDidMount() {
+    this.props.getPost(this.props.match.params.id);
+  }
+
+  render() {
+    const { post, loading } = this.props.post;
+    let PostContent;
+    if (post === null || loading || Object.keys(post).length === 0) {
+      PostContent = <Spinner />;
+    } else {
+      PostContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
+    return (
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {PostContent}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
