cedce51da6c3c560e00a29818b70fc5c70100675
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-19 16:38:24 -0700

    more adjustments made to package.json & server.js

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Serve static assets if in production
+// Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
