122a050aead2816db955a4658dee047d4e900439
Jonathan Funk <jonfunk88@hotmail.com>
2018-08-20 10:22:47 -0700

    commented out redux extension

--- client/src/store.js
+++ client/src/store.js
@@ -9,9 +9,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
   )
 );
 
